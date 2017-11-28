b4d1a07bb1b9500e4996f17fdfd5a33f656cefd5
Jacob Rafati <yrafati@gmail.com>
2017-11-27 21:21:34 -0800

    fixed a bug

--- DenseNet_Cifar_10/DenseNet_compress.py
+++ DenseNet_Cifar_10/DenseNet_compress.py
@@ -590,7 +590,7 @@ with tf.Session() as sess:
 		for layer, _ in w.items():
 			if layer not in ['Variable_39:0']:
 				norm_compression += LA.norm(w[layer] - wC[layer])
-		elif layer == 'Variable_39:0':
+			elif layer == 'Variable_39:0':
 				norm_compression += LA.norm(w[layer][:ref_weights_values[layer].size] - wC[layer])
 
 		print('norm of compression: {} ' .format(norm_compression) )
